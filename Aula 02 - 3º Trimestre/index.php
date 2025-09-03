6792d9ee4537b31c4ef0a16e429ed02d0717224d
Daniel <daniel.abermond@aluno.edu.es.gov.br>
2025-09-03 11:01:18 -0300

    Create index.php

---
+++ Aula 02 - 3º Trimestre/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 02 PHP </title>
+    </head>
+    <body>
+        <?php 
+            $nome1 = "Daniel A.";
+            $nome2 = "Erick";
+
+            echo "Meu nome é: " .$nome1 "E minha dupla é o: " .$nome2;
+        ?>
+    </body>
+</html>

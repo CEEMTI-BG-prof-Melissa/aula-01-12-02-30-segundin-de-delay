ea2caca7038db013c2bf17c80a8a03d101f02e2d
Daniel <daniel.abermond@aluno.edu.es.gov.br>
2025-09-24 09:06:12 -0300

    Create index.php

---
+++ Aula 05 - 3º Trimestre/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title> Aula 05 PHP </title>
+    </head>
+    <body>
+        <?php 
+        $n1 = 2;
+        $n2 = $_GET["x"];
+
+        echo "Os valores informados são $n1 e $n2 <br>";
+        echo "O valor absoluto de $n2 é " .abs($n2);
+        echo "<br> O resultado de $n2 <sup>$n1</sup> é igual a " .pow($n2, $n1);
+        echo "<br> A raiz quadrada de $n2 é " .sqrt($n2);
+        echo "<br> O valor arredondado de $n2 é " .round($n2);
+        echo "<br> O valor de $n2 arredondado pra cima é " .ceil($n2);
+        echo "<br> O valor de $n2 arredondado pra baixo é " .floor($n2);
+        echo "<br> A parte inteira de $n2 é " .intval($n2);
+        ?>
+    </body>
+</html>
